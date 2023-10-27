fcd97cd67b17979f734eeaad5488d80cbe26dc69
zibellino <szabolcs.pall@aol.com>
2023-10-27 15:56:01 +0000

    prepare for gcloud

--- index.js
+++ index.js
@@ -1,4 +1,12 @@
-const express = require(‘express’)
-const app = express()
-app.get(‘/’, (req, res) => res.send(‘Hello World!’))
-app.listen(3000, () => console.log(‘Example app listening on port 3000!’))
+const express = require('express');
+const app = express();
+
+app.get('/', (req, res) => {
+  const name = process.env.NAME || 'World';
+  res.send(`Hello ${name}!`);
+});
+
+const port = parseInt(process.env.PORT) || 8080;
+app.listen(port, () => {
+  console.log(`helloworld: listening on port ${port}`);
+});
