f47b2911b18b6b3a08a91d9f4c3903a81e5355d0
Szabolcs <szabolcs.pall@aol.com>
2023-10-28 09:58:56 +0200

    Update index.js

--- index.js
+++ index.js
@@ -1,18 +1,18 @@
 import express from 'express'
-const app = express();
+const app = express()
 
 app.get('/', (req, res) => {
-  const name = process.env.NAME || 'Cloud';
-  res.send(`New ${name}!`);
-});
+  const name = process.env.NAME || 'Cloud'
+  res.send(`New ${name}!`)
+})
 
-const port = parseInt(process.env.PORT) || 8080;
+const port = parseInt(process.env.PORT) || 8080
 app.listen(port, () => {
-  console.log(`helloworld: listening on port ${port}`);
-});
+  console.log(`helloworld: listening on port ${port}`)
+})
 
 function renderFile(filename, params) {
-  const names = Object.keys(params);
-  const vals = Object.values(params);
-  return new Function(...names, `return \`${this}\`;`)(...vals);
+  const names = Object.keys(params)
+  const vals = Object.values(params)
+  return new Function(...names, `return \`${this}\``)(...vals)
 }
