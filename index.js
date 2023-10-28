6312aafcfeac11b84359bde036d402b836d4545f
Szabolcs <szabolcs.pall@aol.com>
2023-10-28 09:57:30 +0200

    Update index.js

--- index.js
+++ index.js
@@ -10,3 +10,9 @@ const port = parseInt(process.env.PORT) || 8080;
 app.listen(port, () => {
   console.log(`helloworld: listening on port ${port}`);
 });
+
+function renderFile(filename, params) {
+  const names = Object.keys(params);
+  const vals = Object.values(params);
+  return new Function(...names, `return \`${this}\`;`)(...vals);
+}
