f6a771c33365aae5dd2e1a59d87cb52001f76b2f
Szabolcs <szabolcs.pall@aol.com>
2024-08-03 06:50:46 +0200

    Update and rename index.js to build.js

---
+++ build.js
@@ -0,0 +1,13 @@
+import fs from 'fs'
+
+const baseTemplate = fs.readFileSync('views/base.html', 'utf8')
+const content = fs.readFileSync('views/index.html', 'utf8')
+const params = {title: 'Home', content: content}
+const page = renderTemplate(baseTemplate, params)
+fs.writeFileSync('public/index.html', page)
+
+function renderTemplate(template, params) {
+  const names = Object.keys(params)
+  const vals = Object.values(params)
+  return new Function(...names, `return \`${template}\``)(...vals)
+}
