7e707ac48eddd08a973d393afb14c70811f67580
Szabolcs <szabolcs.pall@aol.com>
2024-08-03 23:35:20 +0200

    Update build.js

--- build.js
+++ build.js
@@ -4,6 +4,8 @@ const baseTemplate = fs.readFileSync('views/base.html', 'utf8')
 const content = fs.readFileSync('views/index.html', 'utf8')
 const params = {title: 'Home', content: content}
 const page = renderTemplate(baseTemplate, params)
+
+fs.mkdirSync('public')
 fs.writeFileSync('public/index.html', page)
 fs.cpSync('images', 'public/images', {recursive: true})
 fs.cpSync('style', 'public/style', {recursive: true})
