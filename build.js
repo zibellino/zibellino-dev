bd07b58d68e94a64cce22d6ed0628042caa67891
Szabolcs <szabolcs.pall@aol.com>
2024-08-25 06:43:01 +0200

    Update build.js

--- build.js
+++ build.js
@@ -5,15 +5,7 @@ const pages = ['index', 'music', 'keyboard']
 const languages = ['en', 'it']
 const $ = {
   svg: (name) => fs.readFileSync(`public/images/${name}.svg`),
-  content: (page) => new Function('$', `return \`${fs.readFileSync(`pages/${page || $.page}.html`, 'utf8')}\``)($)
-}
-
-const renderTemplate = (template, params) => {
-  return new Function('$', `return \`${template}\``)(params)
-}
-
-const includeSvg = (name) => {
-  return fs.readFileSync(`public/images/${name}.svg`)
+  html: (page) => new Function('$', `return \`${fs.readFileSync(`pages/${page || $.page}.html`, 'utf8')}\``)($)
 }
 
 pages.forEach(page => {
@@ -23,18 +15,11 @@ pages.forEach(page => {
     if (translations.path && !fs.existsSync(publicPath)) {
       fs.mkdirSync(publicPath)
     }
-
-    const params = {
-      page: page,
-      lang: translations,
-      svg: includeSvg,
-      $: $
-    }
     
     $.page = page
     $.lang = translations
 
-    const renderedPage = $.content('base')
+    const renderedPage = $.html('base')
     fs.writeFileSync(`${publicPath}${page}.html`, renderedPage)
   })
 })
