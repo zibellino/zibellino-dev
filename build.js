3c2eb3967fec63e94a21c4b41f4a4f3c6b1d8b65
Szabolcs <szabolcs.pall@aol.com>
2024-08-29 00:37:16 +0200

    Update build.js

--- build.js
+++ build.js
@@ -5,21 +5,6 @@ const pages = ['index', 'music', 'keyboard']
 const languages = ['en', 'it']
 const translations = []
 
-languages.forEach(language => {
-  translations[language] = JSON.parse(fs.readFileSync(`lang/${language}.json`, 'utf8'))
-
-  if (language !== 'en') {
-    fs.mkdirSync(`public/${language}`)
-  }
-})
-
-const href = (page, lang) => {
-  page = page !== 'index' ? page : ''
-  lang = lang !== 'en' ? lang : ''
-
-  return `/${[lang, page].filter(Boolean).join('/')}`
-}
-
 const $ = {
   svg: (name) => fs.readFileSync(`public/images/${name}.svg`),
   title: (page) => translations[$.lang].titles[page || $.page],
@@ -27,9 +12,15 @@ const $ = {
     const content = fs.readFileSync(`html/${partial ? `partial/${partial}` : $.page}.html`, 'utf8')
     return new Function('$', `return \`${content}\``)(params || $)
   },
+  href: (page, lang) => {
+    page = page !== 'index' ? page : ''
+    lang = lang !== 'en' ? lang : ''
+
+    return `/${[lang, page].filter(Boolean).join('/')}`
+  },
   pageLinks: () => pages.map(page => {
     const params = {
-      href: href(page, $.lang),
+      href: $.href(page, $.lang),
       text: $.title(page),
       rel: page === 'index' ? 'author' : '',
     }
@@ -38,7 +29,7 @@ const $ = {
   }).join(''),
   langLinks: () => languages.map(lang => {
     const params = {
-      href: href($.page, lang),
+      href: $.href($.page, lang),
       text: lang.toUpperCase(),
       rel: 'alternate',
       hreflang: lang,
@@ -48,13 +39,17 @@ const $ = {
   }).join(''),
 }
 
-pages.forEach(page => {
-  languages.forEach(lang => {
+languages.forEach(lang => {
+  translations[lang] = JSON.parse(fs.readFileSync(`lang/${lang}.json`, 'utf8'))
+
+  if (lang !== 'en') {
+    fs.mkdirSync(`public/${lang}`)
+  }
+
+  pages.forEach(page => {
     $.page = page
     $.lang = lang
 
     fs.writeFileSync(`public/${lang !== 'en' ? `${lang}/` : ''}${page}.html`, $.html())
   })
 })
-
-
